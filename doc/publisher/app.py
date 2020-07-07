6a87aecdea9f7c2aaea0dee8581f9b2f70973d48
Yudai.Saito <1029potato@gmail.com>
2020-07-07 13:39:09 +0900

    [Add] Implementation of tabs and menus.
    Created a template for how to make a tab and menu bar in tkinter.
    Create a class and add a function you want to implement to a tab.

--- doc/publisher/app.py
+++ doc/publisher/app.py
@@ -1,2 +1,82 @@
 #coding:utf-8
 
+import tkinter as tk
+from tkinter import ttk
+
+#import pub
+
+
+class Gui(ttk.Notebook):
+    def __init__(self, master):
+        super().__init__(master)
+        note = ttk.Notebook(master)
+
+        port_page = Port(note, "Port")
+        linetrace_page = Trace(note, "LineTrace")
+
+        array = [port_page, linetrace_page]
+
+        for i in array:
+            note.add(i, text=i.ret_name())
+
+        note.pack(fill=tk.BOTH, expand=True) 
+
+
+class Port(tk.Frame):
+    def __init__(self, master=None, txt=None):
+        super().__init__(master)
+        self.name = txt
+        page = tk.Frame(master)
+        self.widget(txt)
+
+    def widget(self, txt):
+        label = tk.Label(self, text=txt, font=("", 30))
+        label.pack()
+    
+    def ret_name(self):
+        return self.name
+
+
+class Trace(tk.Frame):
+    def __init__(self, master=None, txt=None):
+        super().__init__(master)
+        self.name = txt
+        page = tk.Frame(master)
+        self.widget(txt)
+        self.pack()
+    
+    def widget(self, txt):
+        label = tk.Label(self, text=txt, font=("", 30))
+        label.pack()
+    
+    def ret_name(self):
+        return self.name
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("1100x600")
+    root.title("EV3 Control System with GUI")
+
+    app = Gui(master=root)
+    app.pack()
+
+    create_menu(root)
+    
+    root.mainloop()
+
+def create_menu(master):
+    menubar = tk.Menu(master)
+    
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open")
+    filemenu.add_command(label="Save")
+    filemenu.add_command(label="Exit")
+    
+    menubar.add_cascade(label="File", menu=filemenu)
+    
+    master.config(menu=menubar)
+
+
+if __name__ == "__main__":
+    main()
