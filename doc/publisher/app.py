97f73c5f445dafbc8288c484fa629df970a6a0a3
Yudai-Saito <1029potato@gmail.com>
2020-07-14 13:43:38 +0900

    [Move] Collect as Home class.
    remove class Gui & class Menu.
    group the initial screen as Home class.

--- doc/publisher/app.py
+++ doc/publisher/app.py
@@ -8,23 +8,33 @@ from PIL import Image, ImageTk
 
 #import pub
 
-
-class Gui(ttk.Notebook):
+class Home(tk.Menu, ttk.Notebook):
     def __init__(self, master):
         super().__init__(master)
+        self.create_note(master)
+        self.create_menu(master)
+        
+        
+    def create_note(self, master):
         note = ttk.Notebook(master)
 
-        # --- fix this logic later
-        port_page = Port(note, "Port")
-        linetrace_page = Trace(note, "LineTrace")
+        notes = [Port(note, "Port"), Trace(note, "LineTrace")]
 
-        array = [port_page, linetrace_page]
-
-        for i in array:
+        for i in notes:
             note.add(i, text=i.ret_name())
-        # ---
+
 
         note.pack(fill=tk.BOTH, expand=True) 
+
+    def create_menu(self, master):
+        menubar = tk.Menu(master)
+        
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command= lambda: exit())
+
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        master.config(menu=menubar)
 
 
 class Port(tk.Frame):
@@ -37,11 +47,17 @@ class Port(tk.Frame):
 
     def sensor_combobox(self):
 
-        sensor_pic_path = ["../../pic/none.png", "../../pic/color_sensor.jpg", "../../pic/jayro_sensor.jpg", "../../pic/touch_sensor.jpg", "../../pic/ultrasonic_sensor.jpg"]
+        sensor_pic_path = { "none" : "../../pic/none.png", 
+                            "color" : "../../pic/color_sensor.jpg", 
+                            "jayro" : "../../pic/jayro_sensor.jpg", 
+                            "touch" : "../../pic/touch_sensor.jpg", 
+                            "ultrasonic" : "../../pic/ultrasonic_sensor.jpg"
+                            }
         # 設定ボタン押す -> port1変数へ格納 -> subへ送信 -> 送信情報を元にifでport設定
-        
+        sensor_port_name = ["1", "2", "3", "4"]
+
         y = 0.15
-        for i in ["1", "2", "3", "4"]:
+        for i in sensor_port_name:
             label = tk.Label(self, text=i, relief="groove", font=("", 15, "bold"))
             label.place(relx=0.04, rely=y-0.1, relwidth=0.03, relheight=0.05)
 
@@ -62,7 +78,7 @@ class Port(tk.Frame):
         y = 0.1
         for i in range(4):
 
-            sensor_img = Image.open(pathlib.Path(sensor_pic_path[0]))
+            sensor_img = Image.open(pathlib.Path(sensor_pic_path["none"]))
             sensor_img = sensor_img.resize((100,100))
             sensor_img = ImageTk.PhotoImage(sensor_img)
 
@@ -74,8 +90,15 @@ class Port(tk.Frame):
 
     def motor_combobox(self):
 
+        motor_pic_path = { "none" : "../../pic/none.png", 
+                            "large" : "../../pic/large_motor.jpg", 
+                            "midium" : "../../midium_motor.jpg"
+                            }
+
+        sensor_port_name = ["A", "B", "C", "D"]
+
         y = 0.15
-        for i in ["A", "B", "C", "D"]:
+        for i in sensor_port_name:
             label = tk.Label(self, text=i, relief="groove", font=("", 15, "bold"))
             label.place(relx=0.54, rely=y-0.1, relwidth=0.03, relheight=0.05)
 
@@ -86,6 +109,19 @@ class Port(tk.Frame):
             motor_port.place(relx=0.75, rely=y)
             motor_port.current(0)
             motor_port.bind("<<ComboboxSelected>>", (lambda e, motor_port=motor_port: print(motor_port.get())))
+
+            y += 0.23
+
+        y = 0.1
+        for i in range(4):
+
+            sensor_img = Image.open(pathlib.Path(motor_pic_path["none"]))
+            sensor_img = sensor_img.resize((100,100))
+            sensor_img = ImageTk.PhotoImage(sensor_img)
+
+            sensor_img_lbl = tk.Label(self, image=sensor_img)
+            sensor_img_lbl.photo = sensor_img
+            sensor_img_lbl.place(relx=0.6, rely=y)
 
             y += 0.23
 
@@ -108,28 +144,11 @@ class Trace(tk.Frame):
         return self.name
 
 
-class Menu(tk.Menu):
-    def __init__(self, master):
-        super().__init__(master)
-        self.create_menu(master)
-
-    def create_menu(self, master):
-        menubar = tk.Menu(master)
-        
-        filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Exit", command= lambda: exit())
-
-        menubar.add_cascade(label="File", menu=filemenu)
-        
-        master.config(menu=menubar)
-
-
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1100x600")
     root.title("EV3 Control System with GUI")
 
-    app = Gui(master=root)
-    Menu(root)
+    app = Home(master=root)
 
     root.mainloop()
