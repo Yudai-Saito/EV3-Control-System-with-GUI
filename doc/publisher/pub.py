8d4ffc3f9f78520bb36e178c87e9b9bf123fbb54
Yudai-Saito <1029potato@gmail.com>
2020-08-23 21:54:51 +0900

    [Add] Get each element from combobox.

--- doc/publisher/pub.py
+++ doc/publisher/pub.py
@@ -6,6 +6,16 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 
+port_stat = { "port1" : "none", 
+                "port2" : "none",
+                "port3" : "none",
+                "port4" : "none",
+                "portA" : "none",
+                "portB" : "none", 
+                "portC" : "none",
+                "portD" : "none"
+}
+
 def connect_t(button):
     
     button.configure(state="disable")
@@ -24,9 +34,15 @@ def connect_t(button):
         
         messagebox.showinfo("notice", "Connection timeout.")
 
-def set_port(port_info):
+def set_port(sensor_port, motor_port):
 
-    client.publish("topic/motor/dt", str(0) + "," + str(0))
+    port_info = sensor_port + motor_port
+    
+    for key, val in zip(port_stat, port_info):
+        port_stat[key] = val.get()
+    
+    
+    #client.publish("topic/motor/dt", str(0) + "," + str(0))
 
 def connect(button):
 
