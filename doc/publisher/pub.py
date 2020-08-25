a96fe7d0d3a95dcb7a6883daf51e601b65a1c8a8
Yudai-Saito <1029potato@gmail.com>
2020-08-25 09:14:01 +0900

    [Add] Convert the port dict to json and send.

--- doc/publisher/pub.py
+++ doc/publisher/pub.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 
+import json
 import threading
 from tkinter import messagebox
 import paho.mqtt.client as mqtt
@@ -16,10 +17,12 @@ port_stat = { "port1" : ["none", "none"],
                 "portD" : ["none", "none"]
 }
 
+#接続時のフラグと切断時のフラグ管理が必要。 未接続の場合にボタンを押した場合のエラーを防ぐ。
+
 def connect_t(button):
     
     button.configure(state="disable")
-    
+
     try:
         #IPは選択できるように後改変。
         client.connect("192.168.0.1",1883,60)
@@ -36,6 +39,8 @@ def connect_t(button):
 
 def set_port(sensor_port, sensor_mode, motor_port, motor_mode):
 
+    client.publish("test", "set_port")
+
     port_info = sensor_port + motor_port
     port_mode = sensor_mode + motor_mode
     
@@ -43,8 +48,11 @@ def set_port(sensor_port, sensor_mode, motor_port, motor_mode):
         port_stat[key][0] = port.get()
         port_stat[key][1] = mode.get()
     
+    port_stat_json = json.dumps(port_stat)
+
+    client.publish("test", port_stat_json)
     
-    #client.publish("topic/motor/dt", str(0) + "," + str(0))
+    #client.publish("topic/motor/dt", port_stat_json)
 
 def connect(button):
 
